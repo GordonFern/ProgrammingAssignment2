3e8d647fc7e72949c6706a0b4a3c16b4b55ef1b1
GordonFern <gordon.fern@btinternet.com>
2015-02-16 22:19:02 +0000

    Initial Cut of the Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Solution consists of two functions:
+## 1. makeCacheMatrix - creates a special matrix 
+## 2. cacheSolve - takes in a matrix created by makeCacheMatrix as a parameter and solves it.
+##                 if the solution has already been solved then returned the cache results
 
-## Write a short comment describing this function
+
+## function makeCacheMatrix(matrix) (optionally takes an initial matrix)
+##
+## Function that contains member functions
+## $get() - returns the currently stored matrix class(matrix)
+## $set() - sets a new matrix taking as input a matrix class(matrix)
+## $setinv(invmatrix) - sets the Inverse matrix to the parameter invmatrix. NB. It does
+##                      not solve the matrix
+## $getinv() - returns the currently stored inverse matrix as set by $setinv or NULL if it
+##             has yet to be set
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invmatrix) inv <<- invmatrix
+  getinv <- function() inv
+  list( set=set,get=get,setinv=setinv,getinv=getinv)
 
 }
 
-
-## Write a short comment describing this function
+## function cacheSolve(special_matrix)
+##
+## Takes as a parameter a matrix created by makeCacheMatrix
+## and returns the inverse matrix.
+##
+## If the matrix has already been solved (inverse matrix already calculated) then the cached
+## results are returned rather than re-solve the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv<-x$getinv()
+  if( !is.null(inv)) {
+     message("getting cached data")
+     return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data)
+  x$setinv(inv)
+  inv
 }
